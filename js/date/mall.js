cea4f35ce2d3a09159decedc8d7c2649e7039af6
royalzhao <royal_zhaoshuai@163.com>
2017-08-30 23:22:11 +0800

    更新

---
+++ js/date/mall.js
@@ -0,0 +1,15 @@
+var mall=[
+	{id:1,img:'img/foods/foodlist1.jpg',name:'西兰花'},
+	{id:2,img:'img/foods/foodlist2.jpg',name:'清蒸螃蟹'},
+	{id:3,img:'img/foods/foodlist3.jpg',name:'啤酒炸串'},
+	{id:4,img:'img/foods/foodlist4.jpg',name:'红烧肉'},
+	{id:5,img:'img/foods/foodlist5.jpg',name:'香酥虾'},
+	{id:6,img:'img/foods/foodlist6.jpg',name:'菜小丸'},
+	{id:7,img:'img/foods/foodlist7.jpg',name:'披萨'},
+	{id:8,img:'img/foods/foodlist8.jpg',name:'狼牙酥'},
+	{id:9,img:'img/foods/foodlist9.jpg',name:'五谷炒饭'},
+	{id:10,img:'img/foods/foodlist10.jpg',name:'排骨锅'},
+	{id:11,img:'img/foods/foodlist11.jpg',name:'糖醋排骨'},
+	{id:12,img:'img/foods/foodlist12.jpg',name:'糯米丸'}
+
+]
