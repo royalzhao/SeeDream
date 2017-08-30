584d3f5ad7f58f3c041bb8b3398dce7fd6f0553c
royalzhao <royal_zhaoshuai@163.com>
2017-08-30 23:38:59 +0800

    更新

--- SeeDream/SeeDream/js/date/user.js
+++
@@ -1,8 +0,0 @@
-var user=[
-	{id:1,account:'二麻子',password:'123456'},
-	{id:2,account:'赵帅',password:'123456'},
-	{id:3,account:'张三',password:'123456'},
-	{id:4,account:'李四',password:'123456'},
-	{id:5,account:'王五',password:'123456'},
-
-]
